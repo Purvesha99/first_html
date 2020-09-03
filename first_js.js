92b3ce7c591e0c01ae81f9e0b2e2be8ecf4d5faa
Purvesha99 <68986783+Purvesha99@users.noreply.github.com>
2020-09-03 10:12:41 +0530

    Update first_js.js

--- first_js.js
+++ first_js.js
@@ -1,4 +1,7 @@
 function message() {
 	var opened = window.open("");
 	opened.document.write("JavaScript is used to program the behavior of web pages. It is a programming language of the web. It enables dynamic interactivity on websites when applied to an HTML document. It is a weakly typed language"); 
+	let date =new Date();
+	console.log('date');
+
 }
